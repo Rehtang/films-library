57e1cf12cb86b0e0f42db568750abc2d73d99cc3
Rehtang <daniltc23@gmail.com>
2022-04-26 16:33:00 +0300

    Revert "Dev"

--- src/main/resources/liquebase/changeset/20220423-02-init_additional.sql
+++
@@ -1,6 +0,0 @@
-create table "film_recieve"(
-    "film_id" varchar(255) ,
-    "is_recieved" boolean default false,
-    primary key("film_id")
-);
-copy film_recieve("film_id") from '20220423-03-movie_warmup.csv' delimiter ',' CSV;
